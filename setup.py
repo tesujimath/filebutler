cdfc07bcc4a80448d2b32d5208f7f8b637ef9793
Simon Guest <simon.guest@tesujimath.org>
2018-03-13 14:00:05 +1300

    Tweaked build scripts and description.

--- setup.py
+++ setup.py
@@ -4,10 +4,14 @@
 
 from setuptools import setup, find_packages
 
+long_description = """Filebutler is a utility for managing old files in huge directory trees.  Huge means of the order of 100 million files.  The motivation is that find is far too slow on directory trees with this many files.  Even working with filelists generated offline by find can be rather slow for interactive use.  Filebutler improves on this by structuring filelists by age, by size, by owner, and by dataset.
+"""
+
 setup(name='filebutler',
       use_scm_version=True,
       setup_requires=['setuptools_scm', 'setuptools_scm_git_archive'],
       description='Utility for managing old files in large directory structures',
+      long_description=long_description,
       author='Simon Guest',
       author_email='simon.guest@tesujimath.org',
       url='https://github.com/tesujimath/filebutler',
